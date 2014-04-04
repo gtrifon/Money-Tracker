8ef3277aab8ff5fcc638f9b2edddf0470c8ad6c9
Chris Dimoulis <christopherjamesd@gmail.com>
2014-04-04 16:53:41 -0500

    Setup template and static files
    Setup main template and static files. Install bootstrap and
    font-awesome .css and .js files. Setup basic home html templates.

--- moneytracker/moneytracker/urls.py
+++ moneytracker/moneytracker/urls.py
@@ -5,8 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'moneytracker.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'moneytracker.views.home', name='home'),
 )
